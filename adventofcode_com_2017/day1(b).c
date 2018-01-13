4c03240a891588b0989979733493c0670f1a3e83
Zarmiez <33245222+Zarmiez@users.noreply.github.com>
2018-01-13 00:12:03 -0600

    Actualizacion

--- adventofcode_com_2017/day1(b).c
+++ adventofcode_com_2017/day1(b).c
@@ -4,31 +4,27 @@ int main(int argc, char** argv){
    if(argc < 2){
       return 1;
    }
-   FILE input = fopen(argv[1], "r");
-   unsigned int sum = 0,
-                first = 0,
-                current = 0,
-                next = 0;
-   unsigned short int first_time = 1;
+   FILE* input = fopen(argv[1], "r");
+   unsigned int sum = 0;
+   unsigned short int first = 0,
+                      current = 0,
+                      next = 0;
    if(input == NULL){
-      return 1;
+      return 2;
    }
-   while(!feof(input)){
-      fscanf(input, "%1u", &next);
-      if(first_time){
+   while(fscanf(input, "%1hu", &next) != EOF){
+      if(current == 0){
          first = next;
          current = next;
       }
       else{
          if(next == current){
-            sum += next;
+            sum += current;
          }
-         else{
-            current = next;
-         }
+         current = next;
       }
    }
-   if(next == first){
+   if(current == first){
       sum += first;
    }
    printf(">> %u\n", sum);
